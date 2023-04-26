491e6110fd4fc9e416932214a8ba055fe3642327
Jbb29950 <121888619+Jbb29950@users.noreply.github.com>
2023-04-26 18:09:39 +0200

    creation dossier

--- src/Controller/UtilisateurController.php
+++ src/Controller/UtilisateurController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller;
 
-use App\Form\RegistrationFormType;
+
+
+use App\Form\UpdateProfileType;
 use App\Repository\ParticipantRepository;
-use App\Service\Peupler;
-use Cassandra\Type\UserType;
-use Doctrine\Persistence\ObjectManager;
-use http\Client\Curl\User;
-use http\Env\Request;
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -18,12 +18,32 @@ class UtilisateurController extends AbstractController
     #[Route('/utilisateur', name: 'app_utilisateur')]
     public function editionProfil(ParticipantRepository $pr): Response
     {
-        return $this->render('utilisateur/editionProfil.html.twig', [
+        return $this -> render('utilisateur/editionProfil.html.twig', [
             'controller_name' => 'UtilisateurController',
-            'participant'=>$pr->findOneBy(['id'=>$this->getUser()->getUserIdentifier()])
+            'participant' => $pr -> findOneBy(['id' => $this -> getUser() -> getUserIdentifier()])
 
         ]);
     }
 
+    #[Route('/utilisateur/modifier', name: 'modifier_profil')]
+    public function modifierProfil(Request $request, EntityManagerInterface $entityManager): Response
+    {   $user=$this->getUser();
+        $modifierProfilForm = $this -> createForm(UpdateProfileType::class,$user);
+        $modifierProfilForm -> handleRequest($request);
+        dump($user);
+        if ($modifierProfilForm -> isSubmitted() && $modifierProfilForm -> isValid()) {
 
+            $entityManager->persist($user);
+            $entityManager -> flush();
+
+            $this -> addFlash('success', 'Profil modifié avec succès.');
+            dump($user);
+            return $this -> redirectToRoute('app_utilisateur');
+        }
+
+        return $this -> render('utilisateur/modificationProfil.html.twig', [
+
+            'modifierProfil' => $modifierProfilForm -> createView(),
+        ]);
+    }
 }
